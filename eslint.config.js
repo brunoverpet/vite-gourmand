e1fc013cce7614151cfc138cdb23b90ba21f1f82
brunoverpet <bverpet46@gmail.com>
2026-05-29 19:38:22 +0200

    chore: allow kebab-case filename in Inertia

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,14 @@
 import { configApp } from '@adonisjs/eslint-config'
 import { react } from '@adonisjs/eslint-config/react'
 
-export default configApp(...react)
+export default configApp(...react, {
+  files: ['inertia/**/*'],
+  rules: {
+    '@unicorn/filename-case': [
+      'error',
+      {
+        case: 'kebabCase',
+      },
+    ],
+  },
+})
