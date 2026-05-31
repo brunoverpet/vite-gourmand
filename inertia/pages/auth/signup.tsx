69f9b2ba0b35b56393a7d9290fdfb81f0d3d3df9
brunoverpet <bverpet46@gmail.com>
2026-05-31 14:15:37 +0200

    style(pages): upgrade signup with shadcn components and clean home boilerplate

--- inertia/pages/auth/signup.tsx
+++ inertia/pages/auth/signup.tsx
@@ -1,68 +1,83 @@
 import { Form } from '@adonisjs/inertia/react'
+import { Button } from '~/components/ui/button'
+import { Input } from '~/components/ui/input'
 
 export default function Signup() {
   return (
-    <div className="form-container">
+    <div className="bg-accent rounded-2xl p-5">
       <div>
         <h1> Signup </h1>
         <p>Enter your details below to create your account</p>
       </div>
 
-      <div>
-        <Form route="new_account.store">
+      <div className="w-52">
+        <Form route="register.store">
           {({ errors }) => (
             <>
               <div>
-                <label htmlFor="fullName">Full name</label>
-                <input
+                <label htmlFor="lastname">Lastname</label>
+                <Input
                   type="text"
-                  name="fullName"
-                  id="fullName"
-                  data-invalid={errors.fullName ? 'true' : undefined}
+                  name="lastname"
+                  id="lastname"
+                  data-invalid={errors.lastname ? 'true' : undefined}
                 />
-                {errors.fullName && <div>{errors.fullName}</div>}
+                {errors.lastname && <div className="text-destructive">{errors.lastname}</div>}
+              </div>
+
+              <div>
+                <label htmlFor="firstname">firstname</label>
+                <Input
+                  type="text"
+                  name="firstname"
+                  id="firstname"
+                  data-invalid={errors.firstname ? 'true' : undefined}
+                />
+                {errors.firstname && <div className="text-destructive">{errors.firstname}</div>}
               </div>
 
               <div>
                 <label htmlFor="email">Email</label>
-                <input
+                <Input
                   type="email"
                   name="email"
                   id="email"
                   autoComplete="email"
                   data-invalid={errors.email ? 'true' : undefined}
                 />
-                {errors.email && <div>{errors.email}</div>}
+                {errors.email && <div className="text-destructive">{errors.email}</div>}
               </div>
 
               <div>
                 <label htmlFor="password">Password</label>
-                <input
+                <Input
                   type="password"
                   name="password"
                   id="password"
                   autoComplete="new-password"
                   data-invalid={errors.password ? 'true' : undefined}
                 />
-                {errors.password && <div>{errors.password}</div>}
+                {errors.password && <div className="text-destructive">{errors.password}</div>}
               </div>
 
               <div>
                 <label htmlFor="passwordConfirmation">Confirm password</label>
-                <input
+                <Input
                   type="password"
                   name="passwordConfirmation"
                   id="passwordConfirmation"
                   autoComplete="new-password"
                   data-invalid={errors.passwordConfirmation ? 'true' : undefined}
                 />
-                {errors.passwordConfirmation && <div>{errors.passwordConfirmation}</div>}
+                {errors.passwordConfirmation && (
+                  <div className="text-destructive">{errors.passwordConfirmation}</div>
+                )}
               </div>
 
               <div>
-                <button type="submit" className="button">
+                <Button type="submit" className="button">
                   Sign up
-                </button>
+                </Button>
               </div>
             </>
           )}
