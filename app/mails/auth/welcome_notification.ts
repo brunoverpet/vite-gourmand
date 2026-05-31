ed87b649285c9f145be333ee6590aa7b8aa36673
brunoverpet <bverpet46@gmail.com>
2026-05-31 17:06:39 +0200

    feat(auth): add welcome email notification

---
+++ app/mails/auth/welcome_notification.ts
@@ -0,0 +1,19 @@
+import type User from '#models/user'
+import { BaseMail } from '@adonisjs/mail'
+
+export default class WelcomeNotification extends BaseMail {
+  constructor(private user: User) {
+    super()
+  }
+
+  /**
+   * The "prepare" method is called automatically when
+   * the email is sent or queued.
+   */
+  prepare() {
+    this.message
+      .to(this.user.email)
+      .subject('Bienvenue chez Vite & Gourmand')
+      .htmlView('emails/auth/welcome')
+  }
+}
