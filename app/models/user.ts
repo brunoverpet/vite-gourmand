9d284ea5e0ea7616a315d796f55cc2bd0dcfd391
brunoverpet <bverpet46@gmail.com>
2026-05-31 14:12:32 +0200

    feat(db): update user and role models with lucid relationships

--- app/models/user.ts
+++ app/models/user.ts
@@ -2,13 +2,11 @@ import { UserSchema } from '#database/schema'
 import hash from '@adonisjs/core/services/hash'
 import { compose } from '@adonisjs/core/helpers'
 import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
+import { belongsTo } from '@adonisjs/lucid/orm'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
+import Role from '#models/role'
 
 export default class User extends compose(UserSchema, withAuthFinder(hash)) {
-  get initials() {
-    const [first, last] = this.fullName ? this.fullName.split(' ') : this.email.split('@')
-    if (first && last) {
-      return `${first.charAt(0)}${last.charAt(0)}`.toUpperCase()
-    }
-    return `${first.slice(0, 2)}`.toUpperCase()
-  }
+  @belongsTo(() => Role)
+  declare role: BelongsTo<typeof Role>
 }
