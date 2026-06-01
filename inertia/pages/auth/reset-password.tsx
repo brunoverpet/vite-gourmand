8631a899f0d6f3cca94d26596fbfaf9d730b3a26
brunoverpet <bverpet46@gmail.com>
2026-06-01 22:46:44 +0200

    feat(ui): implement forgot-password and reset-password pages with persistent layout

---
+++ inertia/pages/auth/reset-password.tsx
@@ -0,0 +1,18 @@
+import { ResetPasswordForm } from '~/components/reset-password-form'
+import AuthLayout from '~/layouts/auth-layout'
+import type { InertiaProps } from '~/types'
+
+type PageProps = InertiaProps<{ id: string }>
+
+export default function ResetPassword({ id }: PageProps) {
+  return <ResetPasswordForm id={id} />
+}
+
+ResetPassword.layout = (page: React.ReactNode) => (
+  <AuthLayout
+    title="Bienvenue chez Vite & Gourmand."
+    description={<>{/* Se connecter <a href="/login">Se connecter</a> */}</>}
+  >
+    {page}
+  </AuthLayout>
+)
