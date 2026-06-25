60d9c0f0b34a50ffd17e1b0e988c326a517b4e64
brunoverpet <bverpet46@gmail.com>
2026-06-25 19:46:16 +0200

    feat(employees): add employee welcome email on account creation

--- app/services/employees/create_employee_action.ts
+++ app/services/employees/create_employee_action.ts
@@ -1,8 +1,10 @@
 import { Roles } from '#enums/roles'
+import EmployeeWelcomeNotification from '#mails/employees/employee_welcome_notification'
 import { RoleService } from '#services/shared/role_service'
 import { UserService } from '#services/shared/user_service'
 import type { AdminCreateEmployePayload } from '#validators/auth/user'
 import { inject } from '@adonisjs/core'
+import mail from '@adonisjs/mail/services/main'
 
 @inject()
 export class CreateEmployeeAction {
@@ -15,6 +17,8 @@ export class CreateEmployeeAction {
     const roleId = await this.roleService.getRoleId(Roles.EMPLOYE)
     const password = this.userService.createPassword()
     const employe = await this.userService.createEmploye({ ...payload, password }, roleId)
+
+    mail.sendLater(new EmployeeWelcomeNotification(employe))
 
     return { employe, password }
   }
